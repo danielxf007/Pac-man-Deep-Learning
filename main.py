b1890c55c59f2280cbc7c442808e33ac3e7904cf
danielxf007 <daniel.higuita007@gmail.com>
2023-04-01 21:28:19 -0500

    first test for the windows

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import sys
+import retro
+from PyQt5.QtWidgets import QApplication
+from graphics.main_window import MainWindow
+from graphics.game_window import GameWindow
+
 if __name__ == '__main__':
-    pass
+    env = retro.make(game='PacManNamco-Nes')
+    screen = env.reset()
+    app = QApplication(sys.argv)
+    main_window = MainWindow((150, 150), (1100, 700), 'Pac-man')
+    game_window = GameWindow(main_window, (500, 50), (512, 480))
+    game_window.setScreen(screen)
+    main_window.addChild(game_window)
+    sys.exit(app.exec())
