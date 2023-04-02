5be0e4f0e567397b9c607eeef60f1ae73d48a5db
danielxf007 <daniel.higuita007@gmail.com>
2023-04-01 23:05:41 -0500

    testing the new class enviornment

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
-import retro
 from PyQt5.QtWidgets import QApplication
 from graphics.main_window import MainWindow
 from graphics.game_window import GameWindow
+from environments.environment import Environment
 
 if __name__ == '__main__':
-    env = retro.make(game='PacManNamco-Nes')
-    screen = env.reset()
+    env = Environment('PacManNamco-Nes')
+    env.reset()
     app = QApplication(sys.argv)
     main_window = MainWindow((150, 150), (1100, 700), 'Pac-man')
     game_window = GameWindow(main_window, (500, 50), (512, 480))
-    game_window.setScreen(screen)
+    game_window.setScreen(env.getScreen())
     main_window.addChild(game_window)
     sys.exit(app.exec())
