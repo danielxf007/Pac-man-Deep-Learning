b8cafdcc6052141011492afa4ddcd50c1e0d15ce
danielxf007 <daniel.higuita007@gmail.com>
2023-04-01 21:27:57 -0500

    second prototype for the main window

--- graphics/main_window.py
+++ graphics/main_window.py
@@ -1,11 +1,28 @@
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QTimer
 
 class MainWindow(QtWidgets.QMainWindow):
 
-    def __init__(self):
+    def __init__(self, init_pos, size, title):
         super().__init__()
-        self.top = 150
-        self.left = 150
-        self.width = 1100
-        self.height = 700
-        self.title = 'Pac-man AI'
+        self.pos = init_pos
+        self.size = size
+        self.setWindowTitle(title)
+        self.central_widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.setGeometry(*init_pos, *size)
+        self.initTimer()
+        self.children=[]
+        self.show()
+    
+    def initTimer(self):
+        self._timer = QTimer(self)
+        self._timer.timeout.connect(self._update)
+        self._timer.start(100000 // 60) # 60 FPS
+    
+    def addChild(self, child):
+        self.children.append(child)
+        
+    def _update(self):
+        for child in self.children:
+            child._update()
