e51d648b474768c00df606ee110a44440dd9bcf4
danielxf007 <daniel.higuita007@gmail.com>
2023-04-03 21:08:41 -0500

    integrated the windowd with an environment

--- graphics/game_window.py
+++ graphics/game_window.py
@@ -1,25 +1,22 @@
+import numpy as np
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPainter, QBrush, QPen, QPolygonF, QImage, QPixmap
 from PyQt5.QtCore import Qt, QPointF
 
 class GameWindow(QtWidgets.QWidget):
 
-    def __init__(self, parent, init_pos, size):
+    def __init__(self, parent, init_pos, size, env):
         super().__init__(parent)
         self.pos = init_pos
         self.size = size
         self.setGeometry(*init_pos, *size)
         self.setObjectName('game_window')
-        self.screen = None
         self.img_label = QtWidgets.QLabel(self)
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.img_label)
         self.setLayout(self.layout)
+        self.env = env
         self.show()
-    
-    def setScreen(self, screen):
-        self.screen = screen
-        print(screen.shape)
         
     def draw_border(self, painter, size):
         painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
@@ -34,7 +31,8 @@ class GameWindow(QtWidgets.QWidget):
         painter = QPainter()
         painter.begin(self)
         self.draw_border(painter, self.size)
-        original_img = QImage(self.screen, self.screen.shape[1], self.screen.shape[0], QImage.Format_RGB888)
+        screen = self.env.getScreen()
+        original_img = QImage(screen, screen.shape[1], screen.shape[0], QImage.Format_RGB888)
         self.img_label.setGeometry(0, 0, *self.size)
         pixmap = QPixmap(original_img)
         pixmap = pixmap.scaled(*self.size, Qt.IgnoreAspectRatio)
@@ -42,4 +40,8 @@ class GameWindow(QtWidgets.QWidget):
         painter.end()
 
     def _update(self):
+        # Keys correspond with B, NULL, SELECT, START, U, D, L, R, A
+        # index
+        control_input = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0], np.int8)
+        self.env.enterInput(control_input)
         self.update()
