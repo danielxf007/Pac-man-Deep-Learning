17561d98190438001f09b2d8412ac842121a7f8f
danielxf007 <daniel.higuita007@gmail.com>
2023-04-03 21:10:32 -0500

    testing the environment and graphics

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from PyQt5.QtWidgets import QApplication
 from graphics.main_window import MainWindow
 from graphics.game_window import GameWindow
 from environments.environment import Environment
+import numpy as np
 
 if __name__ == '__main__':
     env = Environment('PacManNamco-Nes')
-    env.reset()
     app = QApplication(sys.argv)
     main_window = MainWindow((150, 150), (1100, 700), 'Pac-man')
-    game_window = GameWindow(main_window, (500, 50), (512, 480))
-    game_window.setScreen(env.getScreen())
+    game_window = GameWindow(main_window, (500, 50), (512, 480), env)
     main_window.addChild(game_window)
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
+    env.close()
